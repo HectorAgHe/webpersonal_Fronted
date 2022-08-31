b90ca80d7095c4fc7a8c069f376abdfc8947b393
HectorAgHe <aguilarhector440@gmail.com>
2022-08-31 00:56:45 -0500

    Implementando for de projects

--- Web_Personal/app.py
+++ Web_Personal/app.py
@@ -18,7 +18,35 @@ def contact():
 
 @app.route('/portfolio')
 def portfolio():
-    return render_template('public/portfolio.html')
+    projects = [
+        {
+            'name':'Primer proyecto',
+            'description':'As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the size of a marble, the most beautiful you can imagine. That beautiful, warm....',  
+            'image':'img/home-bg.jpg',
+            'url':'http://google.com'
+        },
+        {
+            'name':'Segundo proyecto',
+            'description':'As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the size of a marble, the most beautiful you can imagine. That beautiful, warm....',
+    
+            'image':'img/about-bg.jpg',
+            'url':'http://www.xataca.com'
+        },
+        {
+            'name':'Primer proyecto',
+            'description':'As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the size of a marble, the most beautiful you can imagine. That beautiful, warm....',  
+            'image':'img/home-bg.jpg',
+            'url':'http://google.com'
+        },
+        {
+            'name':'Segundo proyecto',
+            'description':'As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the size of a marble, the most beautiful you can imagine. That beautiful, warm....',
+    
+            'image':'img/about-bg.jpg',
+            'url':'http://www.xataca.com'
+        },
+    ]
+    return render_template('public/portfolio.html', projects=projects)
 
 
 #################### Rutas ############################
