868061a1c4762dbe406800cc38d82d949f46bd34
HectorAgHe <aguilarhector440@gmail.com>
2022-08-31 12:17:25 -0500

    Procesando tados con form

--- Web_Personal/app.py
+++ Web_Personal/app.py
@@ -1,4 +1,4 @@
-from os import access
+
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -59,11 +59,11 @@ def login():
 def register():
     return render_template('auth/register.html')
 
-@app.route('/welcome')
+@app.route('/welcome', methods=('GET', 'POST'))
 def welcome():
     email = request.args.get('mail')
     password = request.args.get('password')
-    access = {'email': email, 'password':password}
+    access = {'email': email }
 
     return render_template('admin/index.html', user_access= access)
 
