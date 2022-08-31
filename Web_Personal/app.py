30c6025ee825535ece83e0f046df2fb534304d26
HectorAgHe <aguilarhector440@gmail.com>
2022-08-31 16:53:41 -0500

    implementando LoginForm parte-2

--- Web_Personal/app.py
+++ Web_Personal/app.py
@@ -1,12 +1,14 @@
 
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request, url_for
 from flask_wtf import FlaskForm
-from wtforms import StingField, PasswordField, SubmitField, EmailField
+from wtforms import StringField, PasswordField, SubmitField, EmailField
+from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
 
-############# Rutas Public #########################
+
+######### Rutas Public ###########
 @app.route('/')
 def index():
     return render_template('public/index.html')
@@ -24,62 +26,70 @@ def portfolio():
     projects = [
         {
             'name':'Primer proyecto',
-            'description':'As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the size of a marble, the most beautiful you can imagine. That beautiful, warm....',  
+            'description':'As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the size of a marble, the most beautiful you can imagine. That beautiful, warm....',
             'image':'img/home-bg.jpg',
-            'url':'http://google.com'
+            'url': 'https://www.google.com'
         },
         {
             'name':'Segundo proyecto',
             'description':'As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the size of a marble, the most beautiful you can imagine. That beautiful, warm....',
-    
             'image':'img/about-bg.jpg',
-            'url':'http://www.xataca.com'
+            'url': 'https://www.xataka.com'
         },
         {
             'name':'Primer proyecto',
-            'description':'As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the size of a marble, the most beautiful you can imagine. That beautiful, warm....',  
+            'description':'As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the size of a marble, the most beautiful you can imagine. That beautiful, warm....',
             'image':'img/home-bg.jpg',
-            'url':'http://google.com'
+            'url': 'https://www.google.com'
         },
         {
             'name':'Segundo proyecto',
             'description':'As we got further and further away, it [the Earth] diminished in size. Finally it shrank to the size of a marble, the most beautiful you can imagine. That beautiful, warm....',
-    
             'image':'img/about-bg.jpg',
-            'url':'http://www.xataca.com'
+            'url': 'https://www.xataka.com'
         },
     ]
     return render_template('public/portfolio.html', projects=projects)
 
-#################### Formularios de WTForms######################
-
+################# Formularios de WTForms ##################
 class LoginForm(FlaskForm):
-    username = EmailField('Username')
-    password = PasswordField('Password')
+    email = EmailField('Correo', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Ingresar')
 
-#################### Rutas Login ############################
-@app.route('/auth/login')
+############ Rutas Login ############
+@app.route('/auth/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+
+        return render_template('admin/index.html', email=email)
+
     return render_template('auth/login.html', form=form)
 
 @app.route('/auth/register')
 def register():
     return render_template('auth/register.html')
 
-@app.route('/welcome', methods=('GET', 'POST'))
-def welcome():
-    email = request.form['mail']
-    password = request.form['password']
-    access = {'email': email }
+@app.route('/welcome', methods=['GET', 'POST'])
+def welcome(form):
+    
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
 
-    return render_template('admin/index.html', user_access= access)
+        return render_template('admin/index.html', email=email)
+
+    return redirect(url_for('login'))
+
+
 
 @app.errorhandler(404)
 def page_error_not_found(e):
-    return render_template('error/404.html')
+    return render_template('error/404.html'), 404
 
-
-if __name__== '__main__':
+if __name__ == '__main__':
     app.run(debug=True)
