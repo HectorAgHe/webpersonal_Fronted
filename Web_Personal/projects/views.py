1660c96450b271049fed6956cf298eeca235fd49
HectorAgHe <aguilarhector440@gmail.com>
2022-09-01 16:18:29 -0500

    App Projects

---
+++ Web_Personal/projects/views.py
@@ -0,0 +1,12 @@
+###################### Imports de Flask y python #################33
+from flask import render_template, request, Blueprint
+
+project_blueprint = Blueprint('project', __name__)
+
+################## TODO: List Projects #######################
+
+################## TODO: Create New Project #######################
+
+################## TODO: Update a Project #######################
+
+################## TODO: Delete a Project #######################
